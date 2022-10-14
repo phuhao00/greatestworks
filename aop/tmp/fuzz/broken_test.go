1f444c4070154b46d9e1e0657d0ef13ba3f03c30
phuhao00 <hhaou@outlook.com>
2022-10-14 17:33:07 +0800

    fuzz

--- aop/tmp/fuzz/broken_test.go
+++ aop/tmp/fuzz/broken_test.go
@@ -1,1 +1,28 @@
 package fuzz
+
+import (
+	"fmt"
+	"testing"
+)
+
+func FuzzBrokenMethod(f *testing.F) {
+	f.Fuzz(func(t *testing.T, Data string) {
+		BrokenMethod(Data)
+	})
+}
+
+func FuzzMod(f *testing.F) {
+	f.Fuzz(func(t *testing.T, a, b int) {
+		fmt.Println(a / b)
+	})
+}
+
+func FuzzReverse(f *testing.F) {
+	f.Fuzz(func(t *testing.T, a string) {
+		Reverse(a)
+	})
+}
+
+// go test -fuzztime 10s -fuzz  FuzzMod
+
+//https://go.dev/security/fuzz/
