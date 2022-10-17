76c20665f5559c387beb71756c22f9c9093cd4ca
phuhao00 <hhaou@outlook.com>
2022-10-17 19:59:04 +0800

    gateway base model

--- business/server/gateway/client/client.go
+++ business/server/gateway/client/client.go
@@ -1,1 +1,28 @@
 package client
+
+import "github.com/phuhao00/network"
+
+type Server struct {
+	clients map[int64]*network.Client
+}
+
+func (s *Server) AddClient(client *network.Client) {
+	s.clients[client.ConnID] = client
+}
+
+func (s *Server) DelClient(client *network.Client) {
+	delete(s.clients, client.ConnID)
+}
+
+func (s *Server) GetServerCount() int32 {
+	return int32(len(s.clients))
+}
+
+func (s *Server) Loop() {
+
+	for {
+		select {
+		//case Message
+		}
+	}
+}
---
+++ business/server/gateway/launch/conf.go
@@ -0,0 +1,5 @@
+package main
+
+type Conf struct {
+	UpdateInfoCd int64
+}
