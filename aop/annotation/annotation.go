83b497f801e649a13ebf8215c0d507b85425fe66
phuhao00 <hhaou@outlook.com>
2022-11-04 18:53:22 +0800

    annotation

--- aop/annotation/annotation.go
+++ aop/annotation/annotation.go
@@ -1,1 +1,18 @@
 package annotation
+
+import (
+	"google.golang.org/protobuf/proto"
+	"reflect"
+)
+
+type Annotation struct {
+	Prototypes map[uint16]reflect.Type
+}
+
+func (a *Annotation) Register(key uint16, pm proto.Message) {
+	if _, ok := a.Prototypes[key]; ok {
+		return
+	}
+	of := reflect.TypeOf(pm)
+	a.Prototypes[key] = of
+}
