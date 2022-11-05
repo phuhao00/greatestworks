6aa9c478b3d350b0dc1c3943bd8ecf678da6cf0e
phuhao00 <hhaou@outlook.com>
2022-11-05 18:04:41 +0800

    annotation

--- aop/annotation/annotation.go
+++ aop/annotation/annotation.go
@@ -16,3 +16,20 @@ func (a *Annotation) Register(key uint16, pm proto.Message) {
 	of := reflect.TypeOf(pm)
 	a.Prototypes[key] = of
 }
+
+func (a *Annotation) GetProtoType(id uint16) reflect.Type {
+	return a.Prototypes[id]
+}
+
+func (a *Annotation) UnMarshal(id uint16, data []byte) proto.Message {
+	var ret proto.Message
+	if protoType := a.GetProtoType(id); protoType != nil {
+		ret = reflect.New(protoType).Interface().(proto.Message)
+		err := proto.Unmarshal(data, ret)
+		if err != nil {
+			return nil
+		}
+		return ret
+	}
+	return nil
+}
