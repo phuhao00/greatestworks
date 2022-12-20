6ff3b966b25308b83bfd00bdb369e7cdc133be95
phuhao00 <hhaou@outlook.com>
2022-12-20 21:01:41 +0800

    player logic adding

--- business/module/player/config.go
+++ business/module/player/config.go
@@ -1,1 +1,11 @@
 package player
+
+// GameStatus 玩家游戏状态
+type GameStatus uint16
+
+const (
+	Online        GameStatus = iota + 1 //在线状态
+	OffLine                             //离线状态
+	GmManager                           //GM管理
+	OffLineResume                       //离线唤起，不是玩家真正的在线状态
+)
