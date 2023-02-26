8e0d2d429aa3618e292fcd62080d12f75552088a
phuhao00 <hhaou@outlook.com>
2023-02-26 15:12:39 +0800

    rust add

---
+++ business/server/battlerust/target/debug/deps/battlerust.d
@@ -0,0 +1,5 @@
+E:\greatestworks\business\server\battlerust\target\debug\deps\battlerust.exe: src\main.rs
+
+E:\greatestworks\business\server\battlerust\target\debug\deps\battlerust.d: src\main.rs
+
+src\main.rs:
