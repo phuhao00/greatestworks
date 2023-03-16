26534a7470b1bc96f6af339d6e756b9390198f3d
calvin <hhaou@outlook.com>
2023-03-17 00:51:05 +0800

    concurrency friend module write and read action test

---
+++ gre/benchmarks/friend/friend_test.go
@@ -0,0 +1,24 @@
+package friend
+
+import "testing"
+
+//test case
+
+func BenchmarkAddFriend(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+
+	}
+}
+
+func BenchmarkDelFriend(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+
+	}
+}
+
+func BenchmarkManagerFriendRequest(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+
+	}
+}
+
