eecf29dedd84a42709fa663b057be54222309c5f
calvin <hhaou@outlook.com>
2023-03-26 22:04:08 +0800

    logger change

--- aop/logger/log.go
+++ aop/logger/log.go
@@ -1,6 +1,7 @@
 package logger
 
 import (
+	"fmt"
 	"log"
 	"sync"
 
@@ -8,14 +9,56 @@ import (
 )
 
 var (
-	Logger         *spoor.Spoor
+	sp             *spoor.Spoor
 	onceInitLogger sync.Once
 )
 
-func init() {
+type LoggingSetting struct {
+	Dir          string
+	Level        int
+	Prefix       string
+	WriterOption spoor.Option
+}
+
+func SetLogging(setting LoggingSetting) {
 	onceInitLogger.Do(func() {
-		fileWriter := spoor.NewFileWriter("log", 0, 0, 0)
-		l := spoor.NewSpoor(spoor.DEBUG, "", log.Ldate|log.Ltime|log.Lmicroseconds|log.Llongfile, spoor.WithFileWriter(fileWriter))
-		Logger = l
+		var opt spoor.Option
+		if setting.WriterOption == nil {
+			fileWriter := spoor.NewFileWriter(setting.Dir, 0, 0, 0)
+			opt = spoor.WithFileWriter(fileWriter)
+		} else {
+			opt = setting.WriterOption
+		}
+		l := spoor.NewSpoor(spoor.Level(setting.Level), setting.Prefix, log.Ldate|log.Ltime|log.Lmicroseconds|log.Llongfile, opt)
+		sp = l
 	})
+}
+
+// DebugF Log line format: [IWEF]mmdd hh:mm:sLogger.uuuuuu threadid file:line] msg
+func DebugF(f string, args ...interface{}) {
+	if sp.CheckLevel(spoor.DEBUG) {
+		return
+	}
+	sp.Output(2, fmt.Sprintf(spoor.DEBUG.String()+" "+f, args...))
+}
+
+func ErrorF(f string, args ...interface{}) {
+	if sp.CheckLevel(spoor.ERROR) {
+		return
+	}
+	sp.Output(2, fmt.Sprintf(spoor.ERROR.String()+" "+f, args...))
+}
+
+func InfoF(f string, args ...interface{}) {
+	if sp.CheckLevel(spoor.INFO) {
+		return
+	}
+	sp.Output(2, fmt.Sprintf(spoor.INFO.String()+" "+f, args...))
+}
+
+func FatalF(f string, args ...interface{}) {
+	if sp.CheckLevel(spoor.FATAL) {
+		return
+	}
+	sp.Output(2, fmt.Sprintf(spoor.FATAL.String()+" "+f, args...))
 }
