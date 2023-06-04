4f8791b1e2d605db05b08f8ba5d2400edd7c2841
calvin <hhaou@outlook.com>
2023-06-04 10:19:49 +0800

    signal readme adding

--- aop/signal/signal.go
+++ aop/signal/signal.go
@@ -14,13 +14,13 @@ func OnStart(o Owner) {
 	for s := range sig {
 		switch s {
 		case syscall.SIGHUP, syscall.SIGINT, syscall.SIGTERM, syscall.SIGQUIT, syscall.SIGKILL:
-			logger.Logger.InfoF("received signal :%v", s)
+			logger.InfoF("received signal :%v", s)
 			o.Stop()
 		case syscall.SIGABRT:
-			logger.Logger.InfoF("[shutdown command ] received signal :%v", s)
+			logger.InfoF("[shutdown command ] received signal :%v", s)
 			o.Stop()
 		default:
-			logger.Logger.InfoF("received signal :%v", s)
+			logger.InfoF("received signal :%v", s)
 		}
 	}
 	//}()
