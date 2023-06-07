f3484e7cb69c19093a794ee9f8f43ed9fe9c9cd2
calvin <hhaou@outlook.com>
2023-06-08 01:58:38 +0800

    idea  record

--- aop/tmp/slice/s_test.go
+++ aop/tmp/slice/s_test.go
@@ -1,13 +1,11 @@
 package slice
 
 import (
-	"fmt"
-	"reflect"
 	"testing"
 )
 
 func TestSlice(t *testing.T) {
-	//S()
-	v := [3]int{}
-	fmt.Println(reflect.TypeOf(v).Kind())
+	S()
+	//v := [3]int{}
+	//fmt.Println(reflect.TypeOf(v).Kind())
 }
