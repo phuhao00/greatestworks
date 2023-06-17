45fcdf0d029b8ef70624ad7ea3ccde40672186f4
phuhao00 <hhaou@outlook.com>
2023-06-17 15:46:32 +0800

    slice.sort test adding

--- aop/tmp/slice/s_test.go
+++ aop/tmp/slice/s_test.go
@@ -1,6 +1,8 @@
 package slice
 
 import (
+	"fmt"
+	"sort"
 	"testing"
 )
 
@@ -8,4 +10,17 @@ func TestSlice(t *testing.T) {
 	S()
 	//v := [3]int{}
 	//fmt.Println(reflect.TypeOf(v).Kind())
+}
+
+type AA struct {
+	A uint64
+}
+
+func TestSort(t *testing.T) {
+	data := []AA{{1}, {2}, {5}, {31}, {3}}
+	sort.Slice(data, func(i, j int) bool {
+		return data[i].A < data[j].A
+	})
+
+	fmt.Println(data)
 }
